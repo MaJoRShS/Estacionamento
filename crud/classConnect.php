c0dab6804acfdf715b74d986700f00acd5c8de12
MaJoRShS <lukas.mr@hotmail.com>
2020-12-01 16:30:26 -0300

    Ajuste para tentar passar com no travis.ci

--- crud/classConnect.php
+++ crud/classConnect.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 define("user", "root");
@@ -21,27 +20,3 @@ class Connection{
     }
 
 }
-=======
-<?php
-
-define("user", "root");
-define("pass", "Bd@Home2020!");
-
-class Connection{
-
-    public static function connect()
-    {
-        try {
-            $conn = new PDO('mysql:host=localhost;dbname=estacionamento', user, pass);
-            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-
-        } catch (PDOException $e) {
-            print "Error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-
-        return $conn;
-    }
-
-}
->>>>>>> Stashed changes
